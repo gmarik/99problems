5b510249bed8ca1b0f956f87cf010d4d9c950e3a
gmarik <gmarik@gmail.com>
2012-11-21 12:05:50 -0600

    tests

--- Ruby/misc/misc.rb
+++ Ruby/misc/misc.rb
@@ -5,34 +5,43 @@ def rev(number, acc = 0)
 end
 
 
-
 def order(money, change)
-  change(money, change, acc = [], [], money)
-  acc
+  change(money, change, [], [], money)
 end
 
 def change(money, change, acc, q, initial)
   if change.empty? || money < 0.0
-    if q.inject(0.0,&:+) > initial
-        # puts ["%4.2f" % q.inject(&:+), ' = ', q.join('+')].join(' ') 
-        # print " ", q.inject(&:+)
-    end
+    # if q.inject(0.0,&:+) > initial
+    #     puts ["%4.2f" % q.inject(&:+), ' = ', q.join('+')].join(' ') 
+    #     print " ", q.inject(&:+)
+    # end
     q.pop
     return acc
   end
 
   if money == 0.0
     acc << q.dup
-    return acc 
+    return acc
   end
 
   h, *t = change
 
   q << h
 
-  change(money - h, change, acc, q, initial)
+  acc = change(money - h, change, acc, q, initial)
   change(money, t, acc, q, initial)
 end
 
 
+if $0 == __FILE__
 
+  require 'test/unit'
+  include Test::Unit::Assertions
+
+  assert rev(123) == 321
+
+
+  # 
+  assert order(1.25, [0.25, 0.5]) == [[0.25, 0.25, 0.25, 0.25, 0.25], [0.25, 0.25, 0.25, 0.25, 0.5], [0.25, 0.25, 0.25, 0.5, 0.5]]
+
+end
